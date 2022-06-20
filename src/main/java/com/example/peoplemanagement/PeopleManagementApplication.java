f7b245ef450633cfbced320c17e87184818a8f8a
Leo Roettele <leo.roettele@liip.ch>
2022-06-20 11:02:33 +0200

    empty spring boot project

---
+++ src/main/java/com/example/peoplemanagement/PeopleManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.peoplemanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeopleManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PeopleManagementApplication.class, args);
+    }
+
+}
