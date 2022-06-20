f1351905668348218a84d4b6710a2437682daade
Leo Roettele <leo.roettele@liip.ch>
2022-06-20 17:34:17 +0200

    display list of people

--- src/main/java/com/example/peoplemanagement/controller/MainController.java
+++ src/main/java/com/example/peoplemanagement/controller/MainController.java
@@ -1,12 +1,24 @@
 package com.example.peoplemanagement.controller;
 
+import com.example.peoplemanagement.model.Person;
+import com.example.peoplemanagement.repository.PersonRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class MainController {
+    @Autowired
+    private PersonRepository personRepository;
+    public @ResponseBody Iterable<Person> getAllPeople() {
+        return personRepository.findAll();
+    }
     @RequestMapping("/")
-    public String index() {
+    public String index(Model model) {
+        model.addAttribute("people", getAllPeople());
         return "index";
     }
 }
