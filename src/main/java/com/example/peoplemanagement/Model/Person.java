b156ef129c3fb1378a642f1146470b0aa813096a
Leo Roettele <leo.roettele@liip.ch>
2022-06-20 11:37:05 +0200

    create person model

---
+++ src/main/java/com/example/peoplemanagement/Model/Person.java
@@ -0,0 +1,10 @@
+package com.example.peoplemanagement.Model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+public class Person {
+    @Getter @Setter private String name;
+    @Getter @Setter private String surname;
+    @Getter @Setter private String email;
+}
