34eb9db5b2343eddaa2ba210d42301d274eb4f82
Leo Roettele <leo.roettele@liip.ch>
2022-06-27 13:44:37 +0200

    implement add person

--- src/main/java/com/example/peoplemanagement/controller/MainController.java
+++ src/main/java/com/example/peoplemanagement/controller/MainController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
@@ -20,5 +21,17 @@ public class MainController {
     public String index(Model model) {
         model.addAttribute("people", getAllPeople());
         return "index";
+    }
+
+    @RequestMapping("/add")
+    public String add(Model model){
+        model.addAttribute("person", new Person());
+        return "add";
+    }
+
+    @PostMapping("/create")
+    public String add(Person person){
+        personRepository.save(person);
+        return "redirect:/";
     }
 }
